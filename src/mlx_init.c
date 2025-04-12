f231bd5b47250bf8f63e2ed6e10640ad6221042f
Z1rtaee <piresbeatrizwrk@gmail.com>
2025-04-12 12:57:26 +0100

    Test

--- src/mlx_init.c
+++ src/mlx_init.c
@@ -6,7 +6,7 @@
 /*   By: bpires-r <bpires-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 14:45:56 by bpires-r          #+#    #+#             */
-/*   Updated: 2025/04/12 02:33:38 by bpires-r         ###   ########.fr       */
+/*   Updated: 2025/04/12 12:53:06 by bpires-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 
 void	data_init(t_solong *data)
 {
-	printf("gr");
 	data->mlx = mlx_init();
-	printf("after init");
 	data->window = mlx_new_window(data->mlx, WIDTH, HEIGHT, "So Long");
 	data->final_screen.image = mlx_new_image(data->mlx, WIDTH, HEIGHT);
+	data->final_screen.width = WIDTH;
+	data->final_screen.height = HEIGHT;
+	printf("final screen width in data init() -> %d\n final screen height in data init() -> %d\n", data->final_screen.width, data->final_screen.height);
 	data->final_screen.data = mlx_get_data_addr(data->final_screen.image, &data->final_screen.bpp, &data->final_screen.size_line, &data->final_screen.type);
 	load_map(data);
 	load_collectibles(data);
@@ -41,7 +42,8 @@ void	draw_tiles(t_solong *data)
 		y = 0;
 		while (data->map.map[x][y])
 		{
-			printf("X: %d Y: %d, img: %p\n", x * 64, y * 64, data->tile_frame.wall.image);
+			printf("Screen dimensions: width = %d, height = %d\n", data->final_screen.width, data->final_screen.height);
+			printf("WIDTH = %d, HEIGHT = %d\n", WIDTH, HEIGHT);
 			if (data->map.map[x][y] == '1')
 				put_img_to_screen(&data->final_screen, data->tile_frame.wall, x * 64, y * 64);
 			else if (data->map.map[x][y] == '0')
@@ -60,7 +62,7 @@ void	draw_tiles(t_solong *data)
 					put_img_to_screen(&data->final_screen, data->tile_frame.c2.frame_1, x * 64, y * 64);
 				else if (i == 2)
 					put_img_to_screen(&data->final_screen, data->tile_frame.c3.frame_1, x * 64, y * 64);
-				else if (i == 3 || i ==  4)
+				else if (i == 3 || i == 4)
 					put_img_to_screen(&data->final_screen, data->tile_frame.c4.frame_1, x * 64, y * 64);
 			}
 			y++;
@@ -72,8 +74,13 @@ void	draw_tiles(t_solong *data)
 void	pixel_put(t_img *img, int x, int y, int color)
 {
 	char	*pixel;
-	
-	pixel = img->data + (x * img->size_line + y * 4);
+
+	if (x >= img->height || y >= img->width)
+	{
+		//printf("x -> %d\n y -> %d\n", x, y);
+		return ;
+	}
+		pixel = img->data + (x * img->size_line + y * 4);
 	*(unsigned int *)pixel = color;
 }
 
@@ -81,23 +88,24 @@ void	put_img_to_screen(t_img *screen, t_img img, int screen_x, int screen_y)
 {
 	int		x;
 	int		y;
-	int		tmp_y;
+//	int		tmp_y;
 	char	*color;
-
+	
 	x = 0;
 	while (x < img.height)
 	{
 		y = 0;
-		tmp_y = screen_y;
+//		tmp_y = screen_y;
 		while (y < img.width)
 		{
+			//printf("screen -> %p\n img -> %p\n x -> %p\n y -> %p\n", screen, &img, &screen_x, &screen_y);
 			color = img.data + (x * img.size_line + y * 4);
 			if (*(int *)color >= 0)
-				pixel_put(screen, screen_x, tmp_y, *(int *)color);
+				pixel_put(screen, screen_x + x, screen_y + y, *(int *)color);
 			y++;
-			tmp_y++;
+//			tmp_y++;
 		}
 		x++;
-		screen_x++;
+//		screen_x++;
 	}
 }
